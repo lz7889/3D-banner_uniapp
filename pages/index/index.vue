8def09ed6f9338fcdc3e5d50699f8bb9aa93e2ec
lz7889 <lz18716842619@163.com>
2021-07-07 10:30:38 +0800

    aaa

---
+++ pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<carousel :img-list="imgList" url-key="url" @selected="selectedBanner"/>
+</template>
+
+<script>
+	import carousel from '@/components/vear-carousel/vear-carousel'
+	export default {
+		components: {
+			carousel
+		},
+		data() {
+			return {
+				imgList: [{
+					url: 'https://img9.51tietu.net/pic/2019-091200/vgkpidei2tjvgkpidei2tj.jpg',
+					id: 1
+				},{
+					url: 'https://img9.51tietu.net/pic/2019-091200/euzekmi5m23euzekmi5m23.jpg',
+					id: 2
+				},{
+					url: 'https://img9.51tietu.net/pic/2019-091200/143tt0ta4sr143tt0ta4sr.jpg',
+					id: 3
+				},{
+					url: 'https://img9.51tietu.net/pic/2019-091200/ff1vqwm3q33ff1vqwm3q33.jpg',
+					id: 4
+				},]
+			}
+		},
+		methods: {
+			selectedBanner(item, index) {
+				console.log('🥒', item, index)
+			}
+		}
+	}
+</script>
+
